c47f87a60e0bba2ec605fa7e57d59e8c632a1983
Siwek9 <72036867+Siwek9@users.noreply.github.com>
2022-12-16 00:19:57 +0100

    update command

--- src/main/java/com/siwek9/main/ChestEventTabExecutor.java
+++ src/main/java/com/siwek9/main/ChestEventTabExecutor.java
@@ -1,8 +1,8 @@
 package com.siwek9.main;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -25,22 +25,46 @@ public class ChestEventTabExecutor implements TabExecutor {
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
 		String[] opCommands = {"create", "edit", "remove", "start"};
 		String[] playerCommands = {"info"};
-
 		String[] commandsToChoose;
 
 		if (sender.isOp()) {
-			List<String> temp = Stream.concat(Arrays.asList(opCommands).stream(), Arrays.asList(playerCommands).stream()).toList();
-			commandsToChoose = (String[])temp.toArray();
+			commandsToChoose = StringConcat(opCommands, playerCommands);
 		}
 		else {
 			commandsToChoose = playerCommands.clone();
 		}
-		sender.sendMessage(commandsToChoose);
-		//Arrays.sort(commandsToChoose);
- 		// sender.sendMessage("witam");
-		// if (args.length == 1)
-		// 	return Arrays.asList(commandsToChoose);
-		List<String> toReturn = Arrays.asList("siema", "witam");
+		Arrays.sort(commandsToChoose);
+		if (args.length == 1) {
+			return OnlyElementsThatStartsWith(Arrays.asList(commandsToChoose), args[0]);
+		}
+		// List<String> toReturn = Arrays.asList("siema", "witam");
+		return null;
+	}
+
+	String[] StringConcat(String[] firstString, String[] secondString) {
+		String[] toReturn = new String[firstString.length + secondString.length];
+		int arrayCursor = 0;
+		for (String string : firstString) {
+			toReturn[arrayCursor] = string;
+			arrayCursor++;
+		}
+		for (String string : secondString) {
+			toReturn[arrayCursor] = string;
+			arrayCursor++;
+		}
+		
+		return toReturn;
+	}
+
+	List<String> OnlyElementsThatStartsWith(List<String> wholeList, String startString) {
+		List<String> toReturn = new ArrayList<String>();
+
+		for (String oneString : wholeList) {
+			if (oneString.startsWith(startString)) {
+				toReturn.add(oneString);
+			}
+		}
+
 		return toReturn;
 	}
 }
