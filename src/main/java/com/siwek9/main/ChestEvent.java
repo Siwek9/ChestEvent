b0b509866b3a0c4fc27e0df3131bd16fd712b4e0
Siwek9 <72036867+Siwek9@users.noreply.github.com>
2022-12-16 18:30:01 +0100

    trolololo

--- src/main/java/com/siwek9/main/ChestEvent.java
+++ src/main/java/com/siwek9/main/ChestEvent.java
@@ -1,11 +1,26 @@
 package com.siwek9.main;
 
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ChestEvent extends JavaPlugin {
+
+	public FileConfiguration config;
+	public File eventsFile;
+
 	@Override
 	public void onEnable() {
 		getLogger().info("Witaj, świecie!");
+
+		config = this.getConfig();
+		eventsFile = new File(getDataFolder(), "events.yml");
+		
+		config.addDefault("youAreAwesome", true);
+        config.options().copyDefaults(true);
+        saveConfig();
+		
 		this.getCommand("event").setExecutor(new ChestEventTabExecutor(this));
 	}
 }
